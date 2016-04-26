acedecb07db843bb0f131816c199f4d5da8c3f8a
rafael-santiago <voidbrainvoid@gmail.com>
2016-04-26 11:49:39 -0300

    Packet slicer tests

--- src/pcap/src/pktslicer.c
+++ src/pcap/src/pktslicer.c
@@ -6,39 +6,47 @@ struct pkt_field_boundaries {
     size_t start_off, end_off;
     unsigned int mask;
     unsigned int rsh;
+    unsigned size;
 };
 
 // INFO(Santiago): This states the slicer behavior for each relevant packet field.
 //                 The basic "gear" for this kind of "machine" is: ( ( ( pkt + start_off ) & mask ) >> lsh )
 const struct pkt_field_boundaries g_pkt_fields[] = {
-    { "eth.dst",     0,  5, 0xffffffff,  0 },
-    { "eth.src",     6, 11, 0xffffffff,  0 },
-    { "eth.type",   12, 13, 0xffffffff,  0 },
-    { "ip.version", 14, 14, 0x000000f0,  4 },
-    { "ip.ihl",     14, 14, 0x0000000f,  0 },
-    { "ip.tos",     15, 15, 0xffffffff,  0 },
-    { "ip.len",     16, 17, 0xffffffff,  0 },
-    { "ip.id",      18, 19, 0xffffffff,  0 },
-    { "ip.flags",   20, 20, 0x0000e000, 13 },
-    { "ip.fragoff", 20, 21, 0x00001fff,  0 },
-    { "ip.ttl",     22, 22, 0xffffffff,  0 },
-    { "ip.proto",   23, 23, 0xffffffff,  0 },
-    { "ip.chsum",   24, 25, 0xffffffff,  0 },
-    { "ip.src",     26, 29, 0xffffffff,  0 },
-    { "ip.dst",     30, 33, 0xffffffff,  0 },
-    { "tcp.src",    34, 35, 0xffffffff,  0 },
-    { "tcp.dst",    36, 37, 0xffffffff,  0 },
-    { "tcp.seqno",  38, 41, 0xffffffff,  0 },
-    { "tcp.ackno",  42, 45, 0xffffffff,  0 },
-    { "tcp.len",    46, 46, 0x0000f000, 12 },
-    { "tcp.reserv", 46, 47, 0x00000fc0,  4 },
-    { "tcp.flags",  46, 47, 0x0000003f,  0 },
-    { "tcp.window", 48, 49, 0xffffffff,  0 },
-    { "tcp.chsum",  50, 51, 0xffffffff,  0 },
-    { "tcp.urgp",   52, 53, 0xffffffff,  0 }
+    { "eth.dst",     0,  5, 0xffffffff,  0, 6 },
+    { "eth.src",     6, 11, 0xffffffff,  0, 6 },
+    { "eth.type",   12, 13, 0xffffffff,  0, 2 },
+    { "ip.version", 14, 14, 0xf0000000, 28, 1 },
+    { "ip.ihl",     14, 14, 0x0f000000, 24, 1 },
+    { "ip.tos",     15, 15, 0xffffffff,  0, 1 },
+    { "ip.len",     16, 17, 0xffffffff,  0, 2 },
+    { "ip.id",      18, 19, 0xffffffff,  0, 2 },
+    { "ip.flags",   20, 20, 0xe0000000, 29, 1 },
+    { "ip.fragoff", 20, 21, 0x1fff0000, 16, 2 },
+    { "ip.ttl",     22, 22, 0xffffffff,  0, 1 },
+    { "ip.proto",   23, 23, 0xffffffff,  0, 1 },
+    { "ip.chsum",   24, 25, 0xffffffff,  0, 2 },
+    { "ip.src",     26, 29, 0xffffffff,  0, 4 },
+    { "ip.dst",     30, 33, 0xffffffff,  0, 4 },
+    { "tcp.src",    34, 35, 0xffffffff,  0, 2 },
+    { "tcp.dst",    36, 37, 0xffffffff,  0, 2 },
+    { "tcp.seqno",  38, 41, 0xffffffff,  0, 4 },
+    { "tcp.ackno",  42, 45, 0xffffffff,  0, 4 },
+    { "tcp.len",    46, 46, 0xf0000000, 28, 1 },
+    { "tcp.reserv", 46, 47, 0x0fc00000, 22, 1 },
+    { "tcp.flags",  46, 47, 0x003f0000, 16, 1 },
+    { "tcp.window", 48, 49, 0xffffffff,  0, 2 },
+    { "tcp.chsum",  50, 51, 0xffffffff,  0, 2 },
+    { "tcp.urgp",   52, 53, 0xffffffff,  0, 2 }
 };
 
-const size_t g_pkt_fields_size = sizeof(g_pkt_fields[0]) / sizeof(g_pkt_fields);
+const size_t g_pkt_fields_size = sizeof(g_pkt_fields) / sizeof(g_pkt_fields[0]);
+
+static int little_endian();
+
+static int little_endian() {
+    unsigned int m = 0x00000001;
+    return (*(&m) & 1);
+}
 
 void set_pkt_field(const char *field, unsigned char *buf, size_t buf_size, const unsigned int value) {
     size_t p = 0;
@@ -61,23 +69,33 @@ void set_pkt_field(const char *field, unsigned char *buf, size_t buf_size, const
 
 void *get_pkt_field(const char *field, const unsigned char *buf, size_t buf_size, size_t *field_size) {
     size_t p = 0;
-    const unsigned char *buf_end = NULL;
+    const unsigned char *mbuf_end = NULL;
     static unsigned int slice = 0;
+    static unsigned char mbuf[0xffff] = "";
+    static size_t mbuf_size = 0;
     if (field == NULL || buf == NULL) {
         return NULL;
     }
-    buf_end = buf + buf_size;
+    memcpy(mbuf, buf, buf_size);
+    mbuf_size = buf_size;
+    mbuf_end = mbuf + mbuf_size;
     for (p = 0; p < g_pkt_fields_size; p++) {
         if (strcmp(g_pkt_fields[p].name, field) == 0) {
-            if (buf + g_pkt_fields[p].start_off + (g_pkt_fields[p].end_off - g_pkt_fields[p].start_off) > buf_end) {
+            if (mbuf + g_pkt_fields[p].start_off + (g_pkt_fields[p].end_off - g_pkt_fields[p].start_off) > mbuf_end) {
                 return NULL;
             }
-            slice = *((unsigned int *)buf + g_pkt_fields[p].start_off);
             if (field_size != NULL) {
-                *field_size = g_pkt_fields[p].end_off - g_pkt_fields[p].start_off;
+                *field_size = g_pkt_fields[p].size;
             }
-            slice = (slice & g_pkt_fields[p].mask) >> g_pkt_fields[p].rsh;
-            return &slice;
+            if (g_pkt_fields[p].mask != 0xffffffff) {
+                slice = *((unsigned int *)(mbuf + g_pkt_fields[p].start_off));
+                if (little_endian()) {
+                    slice = htonl(slice);
+                }
+                slice = (slice & g_pkt_fields[p].mask) >> g_pkt_fields[p].rsh;
+                return &slice;
+            }
+            return (mbuf + g_pkt_fields[p].start_off);
         }
     }
     return NULL;
--- src/pcap/src/tests/main.c
+++ src/pcap/src/tests/main.c
@@ -2,7 +2,64 @@
 #include "../pktslicer.h"
 
 CUTE_TEST_CASE(pktslicer_tests)
+    char *packet = "\x5c\xac\x4c\xaa\xf5\xb5\x08\x95\x2a\xad\xd6\x4f\x08\x00\x45\x00"
+                   "\x00\x34\xc8\xc5\x40\x00\x3a\x06\xc2\x7f\x17\x2d\xdc\x5e\xc0\xa8"
+                   "\x01\x4b\x00\x50\x04\x59\x60\x26\x26\xa7\xba\x84\x24\x9b\x80\x10"
+                   "\x03\x9c\x97\xcd\x00\x00\x01\x01\x05\x0a\xba\x84\x24\x9a\xba\x84"
+                   "\x24\x9b";
+    size_t packet_size = 66;
+    size_t slice_size = 0;
+    void *slice = NULL;
+    struct expect_slices {
+        const size_t slice_size;
+        const unsigned char *slice;
+        const char *pkt_field;
+    };
+    struct expect_slices slices[] = {
+        { 6, "\x5c\xac\x4c\xaa\xf5\xb5", "eth.dst"    },
+        { 6, "\x08\x95\x2a\xad\xd6\x4f", "eth.src"    },
+        { 2, "\x08\x00",                 "eth.type"   },
+        { 1, "\x04",                     "ip.version" },
+        { 1, "\x05",                     "ip.ihl"     },
+        { 1, "\x00",                     "ip.tos"     },
+        { 2, "\x00\x34",                 "ip.len"     },
+        { 2, "\xc8\xc5",                 "ip.id"      },
+        { 1, "\x02",                     "ip.flags"   },
+        { 2, "\x00\x00",                 "ip.fragoff" },
+        { 1, "\x3a",                     "ip.ttl"     },
+        { 1, "\x06",                     "ip.proto"   },
+        { 2, "\xc2\x7f",                 "ip.chsum"   },
+        { 4, "\x17\x2d\xdc\x5e",         "ip.src"     },
+        { 4, "\xc0\xa8\x01\x4b",         "ip.dst"     },
+        { 2, "\x00\x50",                 "tcp.src"    },
+        { 2, "\x04\x59",                 "tcp.dst"    },
+        { 4, "\x60\x26\x26\xa7",         "tcp.seqno"  },
+        { 4, "\xba\x84\x24\x9b",         "tcp.ackno"  },
+        { 1, "\x08",                     "tcp.len"    },
+        { 1, "\x00",                     "tcp.reserv" },
+        { 1, "\x0010",                   "tcp.flags"  },
+        { 2, "\x03\x9c",                 "tcp.window" },
+        { 2, "\x97\xcd",                 "tcp.chsum"  },
+        { 2, "\x00\x00",                 "tcp.urgp"   }
+    };
+    size_t slices_nr = sizeof(slices) / sizeof(slices[0]), s = 0;
+    size_t b = 0;
 
+    slice = get_pkt_field("unk.field", packet, packet_size, &slice_size);
+    CUTE_ASSERT(slice == NULL);
+    CUTE_ASSERT(slice_size == 0);
+
+    slice = get_pkt_field("unk.field", packet, packet_size, NULL);
+    CUTE_ASSERT(slice == NULL);
+
+    for (s = 0; s < slices_nr; s++) {
+        slice = get_pkt_field(slices[s].pkt_field, packet, packet_size, &slice_size);
+        CUTE_ASSERT(slice_size == slices[s].slice_size);
+        CUTE_ASSERT(slice != NULL);
+        for (b = 0; b < slice_size; b++) {
+            CUTE_ASSERT(((unsigned char *)slice)[b] == slices[s].slice[b]);
+        }
+    }
 CUTE_TEST_CASE_END
 
 CUTE_TEST_CASE(run_tests)
